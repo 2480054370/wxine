f7eff5ef016093ce6cdd3def62c7c353943b6bc9
lubin <895112552@qq.com>
2016-04-21 16:53:16 +0800

    .

--- app/src/main/java/com/wxine/android/CommATab.java
+++ app/src/main/java/com/wxine/android/CommATab.java
@@ -2,6 +2,7 @@ package com.wxine.android;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.CardView;
 import android.support.v7.widget.GridLayoutManager;
@@ -18,6 +19,7 @@ import java.util.List;
 public class CommATab extends Fragment {
 
     private CommAAdapter mAdapter;
+    FloatingActionButton fab;
 
     private String mItemData = "Lorem Ipsum.";
 
@@ -25,7 +27,16 @@ public class CommATab extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.f_comma_recyclerview, container, false);
-        CardView cardView =(CardView) view.findViewById(R.id.cardlist_item);
+        fab = (FloatingActionButton) view.findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                /*Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();*/
+                Intent intent = new Intent(getContext(), CommAddActivity.class);
+                startActivity(intent);
+            }
+        });
         RecyclerView recyclerView = (RecyclerView) view.findViewById(
                 R.id.fragment_list_rv);
 
@@ -40,14 +51,6 @@ public class CommATab extends Fragment {
 
         mAdapter = new CommAAdapter(list);
         recyclerView.setAdapter(mAdapter);
-
-        cardView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent  in = new Intent(getActivity(),CommAddActivity.class);
-                startActivity(in);
-            }
-        });
 
         return view;
 
