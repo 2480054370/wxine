b6395496a5773cb9a677e61ccd5a7ff524b23ec7
huangjunfeng <865079657@qq.com>
2016-04-26 10:24:31 +0800

    hjf

--- app/src/main/java/com/wxine/android/EventActivity.java
+++ app/src/main/java/com/wxine/android/EventActivity.java
@@ -7,11 +7,14 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.PopupWindow;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -33,7 +36,8 @@ public class EventActivity extends AppCompatActivity {
     private List<Integer> image = Arrays.asList(R.drawable.tou, R.drawable.tou, R.drawable.tou, R.drawable.tou, R.drawable.tou, R.drawable.tou, R.drawable.tou, R.drawable.tou, R.drawable.tou, R.drawable.tou);
     private String username = "Lorem Ipsum is simply dummy text of the printing and ";
     private String time = "今天10:00 今天10:00 今天10:00 今天10:00 今天10:00 今天10:00 今天10:00 今天10:00 今天10:00 今天10:00 ";
-    private PopupWindow popupwindow;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -61,128 +65,54 @@ public class EventActivity extends AppCompatActivity {
         recyclerView.setLayoutManager(linearLayoutManager);
         recyclerView.setHasFixedSize(true);
         String[] listItems = username.split(" ");
-        String[] listcontent=time.split(" ");
+        String[] listcontent = time.split(" ");
 
         List<String> list = new ArrayList<String>();
-        List<String> content=new ArrayList<String>();
+        List<String> content = new ArrayList<String>();
         Collections.addAll(list, listItems);
-        Collections.addAll(content,listcontent);
-        mAdapter = new TimelineAdapter(image,list,content);
+        Collections.addAll(content, listcontent);
+        mAdapter = new TimelineAdapter(image, list, content);
         recyclerView.setAdapter(mAdapter);
         mAdapter.setOnItemClickListener(new TimelineAdapter.OnRecyclerViewItemClickListener() {
+
+
             @Override
             public void onItemClick(View view, Info data) {
-
+                Log.d("------------", "sss");
             }
 
-            public void onKQClick(View view,Info da){
-                switch (view.getId()) {
-                    case R.id.btn1:
-                        Button btn1 = (Button)findViewById(R.id.btn1);
-
-                        if (popupwindow != null&&popupwindow.isShowing()) {
-                            btn1.setBackgroundColor(0xffE8E8E8);
-                            popupwindow.dismiss();
-
-                            return;
-                        } else {
-                            initmPopupWindowView();
-                            btn1.setBackgroundColor(0xff979797);
-                            popupwindow.showAtLocation(view, 0, 84, 497);
-                            //popupwindow.showAsDropDown(btn1);
-                        }
-                        break;
-                    default:
-                        break;
-                }
-
-
-                }
-
-            public void onQJClick(View view,Info da){
-                switch (view.getId()) {
-                    case R.id.btn2:
-                        Button btn2 = (Button)findViewById(R.id.btn2);
-                        if (popupwindow != null&&popupwindow.isShowing()) {
-                            btn2.setBackgroundColor(0xffE8E8E8);
-                            popupwindow.dismiss();
-
-                            return;
-                        } else {
-                            initmPopupWindowView2();
-                            btn2.setBackgroundColor(0xff979797);
-                            popupwindow.showAtLocation(view, 0, 84, 497);
-                            //popupwindow.showAsDropDown(btn1);
-                        }
-                        break;
-                    default:
-                        break;
-                }
-
-
+            @Override
+            public void onKQClick(View view, Info da, int position) {
+                Toast.makeText(EventActivity.this,"aaa"+position,Toast.LENGTH_SHORT).show();
             }
 
-            public void onPJClick(View view,Info da){
-                switch (view.getId()) {
-                    case R.id.btn3:
-                        Button btn3 = (Button)findViewById(R.id.btn3);
-
-                        if (popupwindow != null&&popupwindow.isShowing()) {
-                            btn3.setBackgroundColor(0xffE8E8E8);
-                            popupwindow.dismiss();
-
-                            return;
-                        } else {
-                            initmPopupWindowView3();
-                            btn3.setBackgroundColor(0xff979797);
-                            popupwindow.showAtLocation(view, 0, 84, 497);
-                            //popupwindow.showAsDropDown(btn1);
-                        }
-                        break;
-                    default:
-                        break;
+            @Override
+            public void onQJClick(View view, Info da, int position) {
+                RelativeLayout attendance = (RelativeLayout) view.findViewById(R.id.attendance);
+                // i=0,it is visible;i=4,it is invisible;i=0,it is gone;
+                int i = 0;
+                i = attendance.getVisibility();
+                if (i == 8) {
+                    attendance.setVisibility(View.VISIBLE);
+                } else {
+                    attendance.setVisibility(View.GONE);
                 }
+            }
 
-
+            @Override
+            public void onPJClick(View view, Info da, int position) {
+                LinearLayout qingjia = (LinearLayout) view.findViewById(R.id.qingjia);
+                // i=0,it is visible;i=4,it is invisible;i=0,it is gone;
+                int i = 0;
+                i = qingjia.getVisibility();
+                if (i == 8) {
+                    qingjia.setVisibility(View.VISIBLE);
+                } else {
+                    qingjia.setVisibility(View.GONE);
+                }
             }
         });
 
-
-    }
-    public void initmPopupWindowView() {
-
-        // // 获取自定义布局文件pop.xml的视图
-        View customView = getLayoutInflater().inflate(R.layout.popview_item,
-                null, false);
-        // 创建PopupWindow实例,200,150分别是宽度和高度
-        popupwindow = new PopupWindow(customView, 595, 410);
-        // 设置动画效果 [R.style.AnimationFade 是自己事先定义好的]
-        //popupwindow.setAnimationStyle(R.style.AnimationFade);
-
-
     }
 
-    public void initmPopupWindowView2() {
-
-        // // 获取自定义布局文件pop.xml的视图
-        View customView = getLayoutInflater().inflate(R.layout.popview_qinjia,
-                null, false);
-        // 创建PopupWindow实例,200,150分别是宽度和高度
-        popupwindow = new PopupWindow(customView, 595, 410);
-        // 设置动画效果 [R.style.AnimationFade 是自己事先定义好的]
-        //popupwindow.setAnimationStyle(R.style.AnimationFade);
-
-    }
-
-    public void initmPopupWindowView3() {
-
-        // // 获取自定义布局文件pop.xml的视图
-        View customView = getLayoutInflater().inflate(R.layout.popview_pingjia,
-                null, false);
-        // 创建PopupWindow实例,200,150分别是宽度和高度
-        popupwindow = new PopupWindow(customView, 595, 410);
-        // 设置动画效果 [R.style.AnimationFade 是自己事先定义好的]
-        //popupwindow.setAnimationStyle(R.style.AnimationFade);
-
-    }
 }
--- app/src/main/java/com/wxine/android/TimelineAdapter.java
+++ app/src/main/java/com/wxine/android/TimelineAdapter.java
@@ -20,117 +20,121 @@ import com.wxine.android.model.Info;
 
 import java.util.List;
 
-public class TimelineAdapter extends RecyclerView.Adapter<TimelineAdapter.MyViewHolder> implements View.OnClickListener{
-	List<Integer> image; //图片
-	List<String> usename; //名字
-	List<String> time;    //文本
-	private PopupWindow popupwindow;
+public class TimelineAdapter extends RecyclerView.Adapter<TimelineAdapter.MyViewHolder> implements View.OnClickListener {
+    List<Integer> image; //图片
+    List<String> usename; //名字
+    List<String> time;    //文本
+    private PopupWindow popupwindow;
 
-	public TimelineAdapter(List<Integer> image, List<String> usename, List<String> time) {
-		this.image = image;
-		this.usename = usename;
-		this.time = time;
-	}
+    public TimelineAdapter(List<Integer> image, List<String> usename, List<String> time) {
+        this.image = image;
+        this.usename = usename;
+        this.time = time;
+    }
 
-	@Override
-	public MyViewHolder onCreateViewHolder(ViewGroup viewGroup, int i) {
-		View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.listview_item,
-				viewGroup, false);
+    @Override
+    public MyViewHolder onCreateViewHolder(ViewGroup viewGroup, int i) {
+        View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.listview_item,
+                viewGroup, false);
 
-		return new MyViewHolder(view);
-	}
+        return new MyViewHolder(view);
+    }
 
-	@Override
-	public void onBindViewHolder(MyViewHolder myViewHolder, int i) {
-		myViewHolder.icon.setImageResource(image.get(i));
-		myViewHolder.name.setText(usename.get(i));
-		myViewHolder.content.setText(time.get(i));
-	}
+    @Override
+    public void onBindViewHolder(MyViewHolder myViewHolder, int i) {
+        myViewHolder.icon.setImageResource(image.get(i));
+        myViewHolder.name.setText(usename.get(i));
+        myViewHolder.content.setText(time.get(i));
+    }
 
-	@Override
-	public int getItemCount() {
-		return usename == null ? 0 : usename.size();
-	}
+    @Override
+    public int getItemCount() {
+        return usename == null ? 0 : usename.size();
+    }
 
-	@Override
-	public void onClick(View v) {
-		if (mOnItemClickListener != null) {
-			//注意这里使用getTag方法获取数据
-			mOnItemClickListener.onItemClick(v, (Info) v.getTag());
-		}
-	}
+    @Override
+    public void onClick(View v) {
+        if (mOnItemClickListener != null) {
+            //注意这里使用getTag方法获取数据
+            mOnItemClickListener.onItemClick(v, (Info) v.getTag());
+        }
+    }
 
-	public void setOnItemClickListener(OnRecyclerViewItemClickListener listener) {
-		this.mOnItemClickListener = listener;
-	}
+    public void setOnItemClickListener(OnRecyclerViewItemClickListener listener) {
+        this.mOnItemClickListener = listener;
+    }
 
-	class MyViewHolder extends RecyclerView.ViewHolder {
-		ImageView icon;
-		TextView name;
-		TextView content;
-		CardView cardView;
-		ImageView imitem;
-		Button btn1;
-		Button btn2;
-		Button btn3;
-		public MyViewHolder(View itemView) {
-			super(itemView);
-			icon = (ImageView) itemView.findViewById(R.id.image);
-			name = (TextView) itemView.findViewById(R.id.username);
-			content = (TextView)itemView.findViewById(R.id.listview_time);
-			imitem = (ImageView)itemView.findViewById(R.id.im_event);
-			imitem.setOnClickListener(new View.OnClickListener() {
-				@Override
-				public void onClick(View v) {
-					Intent intent = new Intent(content.getContext(), SingActivity.class);
-					content.getContext().startActivity(intent);
-				}
-			});
+    class MyViewHolder extends RecyclerView.ViewHolder {
+        ImageView icon;
+        TextView name;
+        TextView content;
+        CardView cardView;
+        ImageView imitem;
+        Button btn1;
+        Button btn2;
+        Button btn3;
 
-			cardView = (CardView)itemView.findViewById(R.id.relative);
-			cardView.setOnClickListener(new View.OnClickListener() {
-				@Override
-				public void onClick(View v) {
-					Intent intent = new Intent(content.getContext(), Event_content.class);
-					content.getContext().startActivity(intent);
+        public MyViewHolder(final View itemView) {
+            super(itemView);
+            icon = (ImageView) itemView.findViewById(R.id.userimage);
+            name = (TextView) itemView.findViewById(R.id.username);
+            content = (TextView) itemView.findViewById(R.id.listview_time);
+            imitem = (ImageView) itemView.findViewById(R.id.im_event);
+            imitem.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent intent = new Intent(content.getContext(), SingActivity.class);
+                    content.getContext().startActivity(intent);
+                }
+            });
 
-				}
-			});
-			btn1 = (Button)itemView.findViewById(R.id.btn1);
-			btn1.setOnClickListener(new View.OnClickListener() {
-				@Override
-				public void onClick(View v) {
-					mOnItemClickListener.onKQClick(v, (Info) v.getTag());
-				}
-			});
+            cardView = (CardView) itemView.findViewById(R.id.relative);
+            cardView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent intent = new Intent(content.getContext(), Event_content.class);
+                    content.getContext().startActivity(intent);
 
-			btn2 = (Button)itemView.findViewById(R.id.btn2);
-			btn2.setOnClickListener(new View.OnClickListener() {
-				@Override
-				public void onClick(View v) {
-					mOnItemClickListener.onQJClick(v, (Info) v.getTag());
-				}
-			});
+                }
+            });
+            btn1 = (Button) itemView.findViewById(R.id.btn1);
+            btn1.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    mOnItemClickListener.onKQClick(v, (Info) v.getTag(), getLayoutPosition());
+                }
+            });
 
-			btn3 = (Button)itemView.findViewById(R.id.btn3);
-			btn3.setOnClickListener(new View.OnClickListener() {
-				@Override
-				public void onClick(View v) {
-					mOnItemClickListener.onPJClick(v, (Info) v.getTag());
-				}
-			});
-		}
-	}
+            btn2 = (Button) itemView.findViewById(R.id.btn2);
+            btn2.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    mOnItemClickListener.onQJClick(itemView, (Info) v.getTag(), getLayoutPosition());
+                }
+            });
 
-
-	private static OnRecyclerViewItemClickListener mOnItemClickListener = null;
+            btn3 = (Button) itemView.findViewById(R.id.btn3);
+            btn3.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    mOnItemClickListener.onPJClick(itemView, (Info) v.getTag(), getLayoutPosition());
+                }
+            });
+        }
+    }
 
 
-	//define interface
-	public static interface OnRecyclerViewItemClickListener {
-		void onItemClick(View view, Info data);
-		void onKQClick(View view,Info da);
-		void onQJClick(View view,Info da);
-		void onPJClick(View view,Info da);
-	}
+    private static OnRecyclerViewItemClickListener mOnItemClickListener = null;
+
+
+    //define interface
+    public static interface OnRecyclerViewItemClickListener {
+        void onItemClick(View view, Info data);
+
+        void onKQClick(View view, Info da, int position);
+
+        void onQJClick(View view, Info da, int position);
+
+        void onPJClick(View view, Info da, int position);
+    }
 }
