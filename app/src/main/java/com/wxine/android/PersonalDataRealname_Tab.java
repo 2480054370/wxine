bafa98cff574636212675c71593e9a1f96cc7245
lijiajie <l1278945971@163.com>
2016-05-13 11:33:33 +0800

    zz

--- app/src/main/java/com/wxine/android/PersonalDataRealname_Tab.java
+++ app/src/main/java/com/wxine/android/PersonalDataRealname_Tab.java
@@ -11,6 +11,7 @@ import android.os.Bundle;
 import android.provider.MediaStore;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -32,6 +33,7 @@ public class PersonalDataRealname_Tab extends Fragment {
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        mInflater = LayoutInflater.from(getContext());
         view = inflater.inflate(R.layout.personaldata_realname_tab, container, false);
         LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getActivity());
         PersonalRealnameUpdata = (Button) view.findViewById(R.id.PersonalRealnameUpdata);
@@ -52,5 +54,51 @@ public class PersonalDataRealname_Tab extends Fragment {
             }
         });
         return view;
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        //图库
+
+            Uri selectedImage = data.getData();
+            String[] filePathColumn = {MediaStore.Images.Media.DATA};
+
+            Cursor cursor = getActivity().getContentResolver().query(selectedImage,
+                    filePathColumn, null, null, null);
+            cursor.moveToFirst();
+
+            int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+            String picturePath = cursor.getString(columnIndex);
+            cursor.close();
+
+
+            //显示在Gallery视图
+            initData(BitmapFactory.decodeFile(picturePath));
+            initView();
+
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+    //照片的显示
+    private void initData(Bitmap per_photos) {
+        mImgIds = new Bitmap[]{per_photos};
+    }
+
+    private void initView() {
+        mGallery = (LinearLayout) view.findViewById(R.id.id_gallery);
+        for (int i = 0; i < mImgIds.length; i++) {
+            final View personalphoto_item = mInflater.inflate(R.layout.personalphoto_item,
+                    mGallery, false);
+            final ImageView img = (ImageView) personalphoto_item.findViewById(R.id.per_photos);
+            img.setImageBitmap(mImgIds[i]);
+            mGallery.addView(personalphoto_item);
+            //删除照片
+            ImageView remove = (ImageView) personalphoto_item.findViewById(R.id.remove_perimg);
+            remove.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    mGallery.removeView(personalphoto_item);
+                }
+            });
+        }
     }
 }
